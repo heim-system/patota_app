ee5e69f95279ccaa60630ad97e6c9c3f87479e11
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2023-10-24 18:28:16 -0300

    feat: Add invite route

--- lib/core/routes/generate_routes.dart
+++ lib/core/routes/generate_routes.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:patota_app/app/modules/auth/presenter/auth_widget.dart';
 import 'package:patota_app/app/modules/edit/presenter/edit_page.dart';
 import 'package:patota_app/app/modules/home/presenter/home_page.dart';
+import 'package:patota_app/app/modules/invite/presenter/invite_page.dart';
 import 'package:patota_app/app/modules/login/presenter/register/login_register_page.dart';
 import 'package:patota_app/app/modules/patota/domain/entities/i_patota.dart';
 import 'package:patota_app/app/modules/register/presenter/register_page.dart';
@@ -23,6 +24,11 @@ class RouteGenerator {
       case Routes.patotaEdit:
         if (args is IPatota) {
           return MaterialPageRoute(builder: (context) => PatotaEditPage(patota: args));
+        }
+        return MaterialPageRoute(builder: (context) => const HomePage());
+      case Routes.patotaInvite:
+        if (args is IPatota) {
+          return MaterialPageRoute(builder: (context) => PatotaInvitePage(patota: args));
         }
         return MaterialPageRoute(builder: (context) => const HomePage());
       default:
--- lib/core/routes/routes.dart
+++ lib/core/routes/routes.dart
@@ -4,4 +4,5 @@ abstract class Routes {
   static const String loginRegister = '/login/register';
   static const String patotaRegister = '/patota/register';
   static const String patotaEdit = '/patota/edit';
+  static const String patotaInvite = '/patota/invite';
 }
