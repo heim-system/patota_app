485e8edd6278ebc953a939741109744ec77e7148
Paulo Henrique Ferreira <p.h.ferreirah@gmail.com>
2023-11-28 18:55:11 -0300

    fix(Property): Added options property
    - Firebase need this property to execute correctly

--- lib/core/config/config.dart
+++ lib/core/config/config.dart
@@ -1,10 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:patota_app/firebase_options.dart';
 
 import 'doctor.dart';
 
 Future<void> initConfigurations() async {
   WidgetsFlutterBinding.ensureInitialized();
   setup();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 }
